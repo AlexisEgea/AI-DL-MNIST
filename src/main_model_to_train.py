398c2c54fa42a34939a6793fce8057bd913ff601
AlexisEgea <alexisegea@outlook.com>
2024-12-18 01:37:12 +0100

    Updating main to use CNN

--- src/main_model_to_train.py
+++ src/main_model_to_train.py
@@ -3,45 +3,54 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 
 from src.ui.draw_digit_ui import DrawDigitUI
-from src.utils.data import preprocess_data, get_class_output
-from model.MLP import MLP
+from src.utils.data import preprocess_data_mlp, preprocess_data_cnn, get_class_output
+from src.model.MLP import MLP
+from model.CNN import CNN
 
 if __name__ == '__main__':
+    # model = MLP()
+    model = CNN()
+
     # load data
     mnist = tf.keras.datasets.mnist
 
     # the data, split between train and validation sets
     (x_train, y_train), (x_val, y_val) = mnist.load_data()
     # pre process data
-    x_train, y_train, x_val, y_val = preprocess_data(x_train, y_train, x_val, y_val)
+    if model.name == "MLP":
+        x_train, y_train, x_val, y_val = preprocess_data_mlp(x_train, y_train, x_val, y_val)
+    elif model.name == "CNN":
+        x_train, y_train, x_val, y_val = preprocess_data_cnn(x_train, y_train, x_val, y_val)
 
     # init the model
-    mlp = MLP()
     class_output = get_class_output(y_train) # same if we use y_val
-    mlp.init_model(class_output)
+    model.init_model(class_output)
 
     # train
-    history = mlp.train(x_train, y_train, x_val, y_val, mlp.optimiser, mlp.loss)
-    mlp.display_result(history)
+    history = model.train(x_train, y_train, x_val, y_val, model.optimiser, model.loss)
+    model.display_result(history)
+
+    print(model.model.summary())
 
     # one prediction with data from validation dataset
-    image = x_val[6].reshape(28, 28)
+    i = 12
+    image = x_val[i].reshape(28, 28)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.axis('off')
     plt.show()
-    vect_prob = mlp.predict(x_val[12])
-    result, prob = mlp.predict_best_class(vect_prob)
+    vect_prob = model.predict(x_val[i])
+    result, prob = model.predict_best_class(vect_prob)
     print('I am confident about {:.2f}% that this image corresponds to digit {}'.format(prob, result))
 
     # predict by creating the data yourself by drawing the digit
     ui = DrawDigitUI()
-    ui.build_ui(mlp)
+    ui.build_ui(model)
     ui.run()
 
     # save model if it doesn't exist
-    mlp.save_if_model_doesnt_exist()
+    # model.save_if_model_doesnt_exist()
     # or save the model even if it exists
-    # mlp.save_model()
+    model.save_model()
 
 
 
