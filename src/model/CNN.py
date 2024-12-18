88179d4514335c2887770a55763ff8f4ab6ed9b1
AlexisEgea <alexisegea@outlook.com>
2024-12-18 01:29:06 +0100

    Adding cnn neural network

---
+++ src/model/CNN.py
@@ -0,0 +1,37 @@
+import os
+import tensorflow as tf
+from src.model.model import Model
+
+class CNN(Model):
+    def __init__(self):
+        super().__init__("CNN")
+
+        self.save_model_path = os.path.join(os.getcwd(), 'configuration/saved_model/cnn.keras')
+
+    def init_model(self, class_output):
+        self.class_output = class_output
+
+        self.model = tf.keras.Sequential()
+        self.model.add(tf.keras.layers.Conv2D(6, kernel_size=5, strides=1, padding='valid', activation=None))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2, padding='valid'))
+        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.Dense(512, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(class_output, activation='softmax'))
+
+    def train(self, x_train, y_train, x_val, y_val, optimizer, loss):
+        if loss == 'categorical_crossentropy':
+            # if we want to use categorical_crossentropy loss, we have to convert the "y" labels
+            y_train = tf.keras.utils.to_categorical(y_train, num_classes=self.class_output)
+            y_val = tf.keras.utils.to_categorical(y_val, num_classes=self.class_output)
+
+        # compile the model with optimizer and loss
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+        # train model
+        history = self.model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=self.epoch)
+        return history
+
+    def predict(self, image):
+        reshape_image =  image.reshape(1, self.height, self.width, 1)
+        return self.model.predict(reshape_image)
