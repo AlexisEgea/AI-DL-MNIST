eaceecaed3c31f2abbbb159956d4f1cf79ad2401
AlexisEgea <alexisegea@outlook.com>
2024-12-30 02:02:56 +0100

    Adding space

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ from src.ui.draw_digit_ui import DrawDigitUI
 if __name__ == '__main__':
     # model = MLP()
     model = CNN()
+
     model.load_model()
     print(model.model.summary())
 
     ui = DrawDigitUI()
     ui.build_ui(model)
     ui.run()
-
 
