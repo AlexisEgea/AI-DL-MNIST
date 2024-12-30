59e0ccba935094a2acff10833ef1f87eb72cbd09
AlexisEgea <alexisegea@outlook.com>
2024-12-30 01:58:07 +0100

    Updating cnn layers

--- src/model/CNN.py
+++ src/model/CNN.py
@@ -13,14 +13,13 @@ class CNN(Model):
 
         self.model = tf.keras.Sequential()
         # 1 Conv
-        self.model.add(tf.keras.layers.Conv2D(6, kernel_size=5, strides=1, padding='valid', activation=None))
+        self.model.add(tf.keras.layers.Conv2D(6, kernel_size=5, strides=1, padding='valid', activation='relu'))
         self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Activation('relu'))
         self.model.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2, padding='valid'))
+
         # 2 Conv
-        self.model.add(tf.keras.layers.Conv2D(6, kernel_size=5, strides=1, padding='valid', activation=None))
+        self.model.add(tf.keras.layers.Conv2D(6, kernel_size=5, strides=1, padding='valid', activation='relu'))
         self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Activation('relu'))
         self.model.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2, padding='valid'))
 
         self.model.add(tf.keras.layers.Flatten())
